4cae3d448d13c8a598748b5238faed02edf1d578
sujal-kalmegh <sujalkalmegh23@gmail.com>
2026-08-07 08:20:10 +0530

    Simplifying env variables

--- src/main/java/com/formfit/formfit_backend/security/JwtUtil.java
+++ src/main/java/com/formfit/formfit_backend/security/JwtUtil.java
@@ -11,10 +11,10 @@ import java.util.Date;
 
 @Component
 public class JwtUtil {
-    @Value("${jwt.secret}")
+    @Value("${jwt.secret:localSecretKey123456789012345678901234567890123456}")
     private String secret;
 
-    @Value("${jwt.expiration}")
+    @Value("${jwt.expiration:86400000}")
     private long expiration;
 
     private SecretKey getSigningKey(){
